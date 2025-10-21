be269cbddfd9fc6f8ddaf80cac44d6e839bea6a0
chiiipk <chi.pk235482@sis.hust.edu.vn>
2025-10-22 00:12:51 +0700

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,61 +1,28 @@
 
-# def editDistRec(s1, s2, m, n, memo):
-#     if m == 0:
-#         return n
-#     if n == 0:
-#         return m
-#     if memo[m][n] != -1:
-#         return memo[m][n]
-#     if s1[m - 1] == s2[n - 1]:
-#         memo[m][n] = editDistRec(s1, s2, m - 1, n - 1, memo)
-#         return memo[m][n]
-#     memo[m][n] = 1 + min(
-#         editDistRec(s1, s2, m, n - 1, memo),
-#         editDistRec(s1, s2, m - 1, n, memo),
-#         editDistRec(s1, s2, m - 1, n - 1, memo)
-#     )
-#     return memo[m][n]
+def memoiz(s1, s2, m, n, mem):
+    if m == 0:
+        return n
+    if n == 0:
+        return m
+    if mem[m][n] != -1:
+        return mem[m][n]
+    if s1[m - 1] == s2[n - 1]:
+        mem[m][n] = memoiz(s1, s2, m - 1, n - 1, mem)
+        return mem[m][n]
+    mem[m][n] = 1 + min(
+        memoiz(s1, s2, m, n - 1, mem),
+        memoiz(s1, s2, m - 1, n, mem),
+        memoiz(s1, s2, m - 1, n - 1, mem)
+    )
+    return mem[m][n]
 
-# # Wrapper function to initiate the recursive calculation
-# def editDistance(s1, s2):
-#     m, n = len(s1), len(s2)
-#     memo = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
-#     return editDistRec(s1, s2, m, n, memo)
+# Wrapper function to initiate the recursive calculation
+def minED(s1, s2):
+    m, n = len(s1), len(s2)
+    mem = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
+    return memoiz(s1, s2, m, n, mem)
 
-# if __name__ == "__main__":
-#     s1 = "I'm fine"
-#     s2 = "Im"
-#     print(editDistance(s1, s2))
-def lcs_tables(X, Y):
-    m, n = len(X), len(Y)
-    c = [[0]*(n+1) for _ in range(m+1)]
-    b = [[""]*(n+1) for _ in range(m+1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
-                c[i][j] = c[i-1][j-1] + 1
-                b[i][j] = "↖"
-            elif c[i-1][j] >= c[i][j-1]:
-                c[i][j] = c[i-1][j]
-                b[i][j] = "↑"
-            else:
-                c[i][j] = c[i][j-1]
-                b[i][j] = "←"
-    return c, b
-
-def print_lcs(b, X, i, j):
-    if i == 0 or j == 0:
-        return []
-    if b[i][j] == "↖":
-        return print_lcs(b, X, i-1, j-1) + [X[i-1]]
-    elif b[i][j] == "↑":
-        return print_lcs(b, X, i-1, j)
-    else:  # "←"
-        return print_lcs(b, X, i, j-1)
-
-# demo
-X = list("ABCBDAB")
-Y = list("BDCABA")
-c, b = lcs_tables(X, Y)
-lcs_seq = "".join(print_lcs(b, X, len(X), len(Y)))
-print(c, b, len(lcs_seq), lcs_seq)  # -> 4 BCBA (một LCS hợp lệ)
+if __name__ == "__main__":
+    s1 = "I'm fine"
+    s2 = "Im fine"
+    print(minED(s1, s2))
